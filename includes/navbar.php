03ae76cf37f74ca7e26f096efc6a9f4141ba44f4
PatandyaW <patandyawisnu@gmail.com>
2023-07-04 23:08:51 +0700

    first

--- includes/navbar.php
+++ includes/navbar.php
@@ -12,7 +12,7 @@
       <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
         <ul class="nav navbar-nav">
           <li><a href="index.php">HOME</a></li>
-          <li><a href="index.php">ABOUT US</a></li>
+          <li><a href="index.php">ASK ABOUT ANYTHING</a></li>
           <li><a href="category_view.php">PRODUCT</a></li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">CATEGORY <span class="caret"></span></a>
